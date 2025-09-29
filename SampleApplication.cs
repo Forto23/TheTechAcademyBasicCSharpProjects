3347c4fc8774f9018e22cbd34da24811efe953d9
Forto23 <fortfernando2328@gmail.com>
2025-09-29 10:06:43 -0700

    Add Daily Report assignment

---
+++ SampleApplication.cs
@@ -0,0 +1,12 @@
+using System;
+
+class SampleApplication
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("This is my first C# program!");
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
